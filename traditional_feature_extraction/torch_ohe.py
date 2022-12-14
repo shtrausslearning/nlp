f5b49a6af55947aaad87b596143244efacfaa7bb
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-12-15 01:11:40 +0300

    Update torch_ohe.py

--- traditional_feature_extraction/torch_ohe.py
+++ traditional_feature_extraction/torch_ohe.py
@@ -4,19 +4,31 @@ from transformers import AutoTokenizer
 # Input Text
 text = 'Tokenising text is a core task of NLP.'
 
+''' Working w/ Tokeniser '''
+
 # AutoTokeniser Subword Tokenisation
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 encoded_text = tokenizer(text)
-print(encoded_text['input_ids'])
-print(encoded_text['attention_mask'],'\n')
+print(encoded_text['input_ids'])            # encoded values
+print(encoded_text['attention_mask'],'\n')  # encoded values mask (for multiple documents)
 
 print('')
 print(f'Vocab size: {tokenizer.vocab_size}')
 print(f'Max length: {tokenizer.model_max_length}')
 print(f'Tokeniser model input names: {tokenizer.model_input_names}')
 
+print('\nConvert IDs to Tokens')
+tokens = tokenizer.convert_ids_to_tokens(encoded_text.input_ids)
+print(tokens,'\n')
+
+print('Convert tokens to string')
+print(tokenizer.convert_tokens_to_string(tokens),'\n')
+
+
+''' Generate Vocabulary '''
+
 # generate token dictionary
-print('token dictionary')
+print('Token dictionary')
 token2id = {ch: idx for idx,ch in enumerate(sorted(set(tokens)))}
 print(token2id)
 
@@ -34,3 +46,38 @@ print(f' OHE size: {one_hot_encodings.shape}')
 
 one_hot_encodings.numpy()  # to numpy array
 one_hot_encodings.tolist() # to list
+
+# [101, 19204, 9355, 3793, 2003, 1037, 4563, 4708, 1997, 17953, 2361, 1012, 102]
+# [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] 
+
+
+# Vocab size: 30522
+# Max length: 512
+# Tokeniser model input names: ['input_ids', 'attention_mask']
+
+# Convert IDs to Tokens
+# ['[CLS]', 'token', '##ising', 'text', 'is', 'a', 'core', 'task', 'of', 'nl', '##p', '.', '[SEP]'] 
+
+# Convert tokens to string
+# [CLS] tokenising text is a core task of nlp. [SEP] 
+
+# Token dictionary
+# {'##ising': 0, '##p': 1, '.': 2, '[CLS]': 3, '[SEP]': 4, 'a': 5, 'core': 6, 'is': 7, 'nl': 8, 'of': 9, 'task': 10, 'text': 11, 'token': 12}
+
+#  token2id 
+#  [3, 12, 0, 11, 7, 5, 6, 10, 9, 8, 1, 2, 4] 
+
+#  OHE size: torch.Size([13, 13])
+# [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+#  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+#  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+#  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+#  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+#  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+#  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+#  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+#  [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]]
