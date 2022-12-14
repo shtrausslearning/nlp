f348d874602bd5dd1e17cb3a8c8664dc4fe609e6
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-12-15 01:04:13 +0300

    Update and rename traditional_features/readme.md to traditional_feature_extraction/torch_ohe.py

---
+++ traditional_feature_extraction/torch_ohe.py
@@ -0,0 +1,31 @@
+
+from transformers import AutoTokenizer
+
+# Input Text
+text = 'Tokenising text is a core task of NLP.'
+
+# AutoTokeniser Subword Tokenisation
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+encoded_text = tokenizer(text)
+print(encoded_text['input_ids'])
+print(encoded_text['attention_mask'],'\n')
+
+# generate token dictionary
+print('token dictionary')
+token2id = {ch: idx for idx,ch in enumerate(sorted(set(tokens)))}
+print(token2id)
+
+# generate ids for tokens via vocab dictionary
+input_ids = [token2id[token] for token in tokens]
+
+print('\n','token2id','\n',input_ids,'\n')
+
+import torch
+import torch.nn.functional as F
+
+inputs_ids = torch.tensor(input_ids)
+one_hot_encodings = F.one_hot(inputs_ids,num_classes = len(token2id))
+print(f' OHE size: {one_hot_encodings.shape}')
+
+one_hot_encodings.numpy()  # to numpy array
+one_hot_encodings.tolist() # to list
