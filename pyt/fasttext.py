3e6d9bf2f5b1c351f33f1f48156ea806ab2086d3
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2023-03-23 10:19:46 +0300

    Update fasttext.py

--- pyt/fasttext.py
+++ pyt/fasttext.py
@@ -1,6 +1,9 @@
 
 # Example of Tast text model 
 
+import torch.nn as nn 
+import torch.nn.functional as f
+
 class FastText(nn.Module):
     
     def __init__(self,vocab_size,embed_dim,num_class):
@@ -17,8 +20,19 @@ class FastText(nn.Module):
         x = self.softmax(x) # non linear activation function
         return x
         
+
+class FastText(nn.Module):
+    
+    def __init__(self, vocab_size, embed_dim, output_dim, pad_idx):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size,embed_dim,padding_idx=pad_idx)
+        self.fc = nn.Linear(embed_dim, output_dim)
         
-        
+    def forward(self, text):
+        embedded = self.embedding(text) 
+        embedded = embedded.permute(1,0,2) 
+        pooled = f.avg_pool2d(embedded,(embedded.shape[1], 1)).squeeze(1)     
+        return self.fc(pooled) 
             
       
     
